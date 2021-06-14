e9489d6d9810b4cd157a17779442b4f76499568a
prateekchandrakar <85864316+prateekchandrakar@users.noreply.github.com>
2021-06-14 21:10:24 +0530

    Add files via upload

---
+++ about_us_section.php
@@ -0,0 +1,33 @@
+
+	
+    <!-- CONTENT START -->
+    <section style="padding-top:40px;">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="home-about">
+                        <h5 class="subtitle">About Company Name</h5>
+                        <h2>The Secret is at the Table and The Natural Light Food</h2>
+                        <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis lorem augue, at quam finibus eget.</strong> </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis lorem augue, at quam finibus eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis lorem augue, at quam finibus eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis lorem augue, at quam finibus eget. </p>
+                        <b><a href="" class="rmore">Read More</a></b>
+                    </div>
+                </div>
+                <div class="col-lg-6 spacing-md">
+                    <div class="row">
+                        <div class="col-md-6 col-6">
+                            <figure class="grid-img">
+                                <img src="images/about2.jpg" alt="" style="box-shadow: 0 10px 15px 0 rgb(3 35 68 / 10%); border-radius:5px;">
+                            </figure>
+                        </div>
+                        <div class="col-md-6 col-6 spacing-sm">
+                            <figure class="grid-img">
+                                <img src="images/commons/restaurant-9.jpg" alt=""  style="box-shadow: 0 10px 15px 0 rgb(3 35 68 / 10%); border-radius:5px;">
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
---
+++ header_section.php
@@ -0,0 +1,347 @@
+<!-- PRELOADER START -->
+    <!--<div id="loader-wrapper">
+        <div class="loader">
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+        </div> 
+    </div>--->
+    <!-- PRELOADER END -->
+
+    <header>
+        <!-- TOP HEADER START -->
+        <div class="top-header-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="top-contact text-left">
+                            <li class="phone"><span class="tel-no">
+								<a href="tel:(01483) 282282"> (01483) 282282</a>, 
+								<a href="tel:(01483) 283283"> (01483) 283283</a>
+								</span></li>
+                            <!--<li class="email"><a href="mailto:info@jalao.com">info@companyname.com</a></li>-->
+                        </ul>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <div class="top-social">
+                            <ul class="social-list">
+                                <li><a href="https://www.facebook.com/yasminrestauranteasthorsley/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#" target="_blank"><i class="fab fa-google-plus"></i></a></li>
+								<li><a href="#" target="_blank"><i class="fab fa-behance"></i></a></li>                                
+                                <!--<li><a href="#"><i class="fab fa-instagram"></i></a></li>-->
+                                <li class="m-0"><a class="btn quote-btn" href="login_register.php" role="button" style="background:#46C756;">Login/ SignIn</a></li>
+								<li class="m-0"><a class="btn quote-btn" href="book_a_table.php" role="button" style="margin-left: 5px;">BOOK TABLE NOW</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="top-header">
+        </div>
+        <!-- TOP HEADER END -->
+        
+        <!-- NAV START -->
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a href="index.php" class="navbar-brand"><img src="images/mainlogo.png" alt=""></a>
+
+                <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#main-nav">
+                    <span class="menu-icon-bar"></span>
+                    <span class="menu-icon-bar"></span>
+                    <span class="menu-icon-bar"></span>
+                </button>
+
+                <div id="main-nav" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        
+                        <li><a href="index.php" class="nav-item nav-link">Home</a></li>
+						<li><a href="about_us.php" class="nav-item nav-link">About</a></li>
+						<li><a href="menu_online.php" class="nav-item nav-link">Menu</a></li>
+						<li><a href="#" class="nav-item nav-link">Order Online</a></li>
+						<li><a href="#" class="nav-item nav-link">Feedback</a></li>
+						<li><a href="#" class="nav-item nav-link">Table Reservation</a></li>
+                        <!--<li class="dropdown">
+                            <a href="#" class="nav-item nav-link" data-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu">
+                                <a href="#" class="dropdown-item">m1</a>
+                                <a href="#" class="dropdown-item">m2</a>
+                                <a href="#" class="dropdown-item">m3</a>
+                                <a href="#" class="dropdown-item">m4</a>
+                            </div>
+                        </li>-->
+                        
+                        <li><a href="contact_us.php" class="nav-item nav-link last-link-item">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- NAV END -->
+		
+<!--responsive code--->
+<style>
+@media (min-width: 320px){
+
+.pages-hero
+	{
+		height:300px !important;
+	}
+.pages-title h1 {
+    
+    margin-top: 10px !important;
+    font-size: 25px !important; line-height: 25px !important;
+}
+
+.pages-title p{
+	font-size:13px !important;
+}
+	
+	.bookTabImg{
+		display:none;
+	}
+	.form_Section{
+		width:100%;  position: unset;   padding: 10px;
+	}
+	
+	.batab {
+    color: #fff;
+    font-family: cursive;
+    font-size: 22px;
+    margin-bottom: 18px;
+    margin-top: 22px;
+}
+
+.subBtnFinal{
+	margin-bottom:20px;
+}	
+
+.cpCls1 {
+    float: none;
+    text-align: center;
+}
+
+}
+@media (min-width: 480px){
+
+.pages-hero
+	{
+		height:300px !important;
+	}
+
+.pages-title h1 {
+    
+    margin-top: 10px !important;
+    font-size: 25px !important; line-height: 25px !important;
+}
+
+.pages-title p{
+	font-size:13px !important;
+}
+	
+	.bookTabImg{
+		display:none;
+	}
+	.form_Section{
+		width:100%;  position: unset;   padding: 10px;
+	}
+	
+	.batab {
+    color: #fff;
+    font-family: cursive;
+    font-size: 22px;
+    margin-bottom: 18px;
+    margin-top: 22px;
+}
+
+.subBtnFinal{
+	margin-bottom:20px;
+}	
+
+
+.cpCls1 {
+    float: none;
+    text-align: center;
+}
+
+
+}
+@media (min-width: 640px){
+
+.pages-title h1 {
+    
+    margin-top: 10px !important;
+    font-size: 25px !important; line-height: 25px !important;
+}
+.pages-title p{
+	font-size:13px !important;
+}
+
+.pages-hero
+	{
+		height:300px !important;
+	}
+	
+	.bookTabImg{
+		display:none;
+	}
+	.form_Section{
+		width:100%;  position: unset;   padding: 10px;
+	}
+	
+	.batab {
+    color: #fff;
+    font-family: cursive;
+    font-size: 22px;
+    margin-bottom: 18px;
+    margin-top: 22px;
+}
+
+.subBtnFinal{
+	margin-bottom:20px;
+}	
+
+.cpCls1 {
+    float: none;
+    text-align: center;
+}
+
+
+}
+@media (min-width: 760px){
+
+.pages-title h1 {
+    
+    margin-top: 60px !important;
+    font-size: 33px !important; line-height: 45px !important;
+}
+
+.pages-title p{
+	font-size:16px !important;
+}
+
+.pages-hero
+	{
+		height:450px !important;
+	}
+	
+	.bookTabImg{
+		display:block;
+	}
+	.form_Section{
+		width:90%;  position: absolute;   padding: 40px;
+	}
+	
+	.batab{
+		    color: #fff;
+    font-family: cursive;
+    font-size: 30px;
+    margin-bottom: 35px;
+	margin-top: 0px;
+	}
+	
+	.subBtnFinal{
+	margin-bottom:0px;
+}	
+
+.cpCls1 {
+    float: right;
+    text-align: center;
+}
+
+}
+@media (min-width: 920px){
+	
+	.pages-title h1 {
+    
+    margin-top: 60px !important;
+    font-size: 33px !important; line-height: 45px !important;
+}
+
+.pages-title p{
+	font-size:16px !important;
+}
+
+.pages-hero
+	{
+		height:450px !important;
+	}
+	
+	.bookTabImg{
+		display:block;
+	}
+	.form_Section{
+		width:90%;  position: absolute;   padding: 40px;
+	}
+	
+	.batab{
+		    color: #fff;
+    font-family: cursive;
+    font-size: 30px;
+    margin-bottom: 35px;
+	margin-top: 0px;
+	}
+
+.subBtnFinal{
+	margin-bottom:0px;
+}	
+
+.cpCls1 {
+    float: right;
+    text-align: center;
+}
+
+	
+}
+@media (min-width: 1200px){
+
+
+.pages-title h1 {
+    
+    margin-top: 60px !important;
+    font-size: 33px !important;     line-height: 45px !important;
+}
+
+.pages-title p{
+	font-size:16px !important;
+}
+	
+	.pages-hero
+	{
+		height:450px !important;
+	}
+	
+	.bookTabImg{
+		display:block;
+	}
+	.form_Section{
+		width:90%;  position: absolute;   padding: 40px;
+	}
+	
+	.batab{
+		    color: #fff;
+    font-family: cursive;
+    font-size: 30px;
+    margin-bottom: 35px;
+	margin-top: 0px;
+	}
+
+.subBtnFinal{
+	margin-bottom:0px;
+}
+
+.cpCls1 {
+    float: right;
+    text-align: center;
+}
+	
+	
+}
+</style>
+<!--responsive code--->		
